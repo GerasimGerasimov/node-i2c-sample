204adf797cd0aaebc23caa5016d13805492bd7ec
GerasimGerasimov <dialix@yandex.ru>
2024-07-19 18:02:41 +0700

    Применил спец библиотеку https://github.com/fivdi/i2c-bus.git

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-import fs = require ("fs/promises");
+import i2c = require('i2c-bus');
 import { delay } from "./utils/delay";
 
 (async () => { 
+  const smb = await i2c.openPromisified(2);
   while (true) {
     try {
-      const i2c = await fs.open ('/dev/i2c-1', 'r+');
       while (true) {
-        //await fs.writeFile('/dev/i2c-1',new Uint8Array([0x20, 0x55]));
-        await i2c.writeFile(new Uint8Array([0x20, 0x55]));
+        await smb.i2cWrite (0x20, 1, Buffer.from([0x55]));
         await delay(1000);
-        await i2c.writeFile(new Uint8Array([0x20, 0xAA]));
+        await smb.i2cWrite (0x20, 1, Buffer.from([0xAA]));
         await delay(1000);
       }
     } catch (e) {
       await delay(1000);
       console.log('Some Error',e);
+      smb.close();
     }
   }
 })();
