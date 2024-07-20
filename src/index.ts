863eaf2ca6460ab1bd52ae905a95cb5ed7dddf15
GerasimGerasimov <dialix@yandex.ru>
2024-07-20 15:53:18 +0700

    исправил адреса в Readme сдедал моргалку на три линейки светодиодов

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,34 @@ import i2c = require('i2c-bus');
 import { delay } from "./utils/delay";
 
 (async () => { 
+  console.log('i2C-sample started')
   const smb = await i2c.openPromisified(2);
   while (true) {
     try {
       while (true) {
         await smb.i2cWrite (0x20, 1, Buffer.from([0x55]));
+        await smb.i2cWrite (0x22, 1, Buffer.from([0x55]));
+        await smb.i2cWrite (0x24, 1, Buffer.from([0x55]));
         await delay(1000);
         await smb.i2cWrite (0x20, 1, Buffer.from([0xAA]));
+        await smb.i2cWrite (0x22, 1, Buffer.from([0xAA]));
+        await smb.i2cWrite (0x24, 1, Buffer.from([0xAA]));
+        await delay(1000);
+        await smb.i2cWrite (0x20, 1, Buffer.from([0xFF]));
+        await smb.i2cWrite (0x22, 1, Buffer.from([0xFF]));
+        await smb.i2cWrite (0x24, 1, Buffer.from([0xFF]));
+        await delay(500);
+        await smb.i2cWrite (0x20, 1, Buffer.from([0x00]));
+        await smb.i2cWrite (0x22, 1, Buffer.from([0x00]));
+        await smb.i2cWrite (0x24, 1, Buffer.from([0x00]));
+        await delay(500);
+        await smb.i2cWrite (0x20, 1, Buffer.from([0xFF]));
+        await smb.i2cWrite (0x22, 1, Buffer.from([0xFF]));
+        await smb.i2cWrite (0x24, 1, Buffer.from([0xFF]));
+        await delay(500);
+        await smb.i2cWrite (0x20, 1, Buffer.from([0x00]));
+        await smb.i2cWrite (0x22, 1, Buffer.from([0x00]));
+        await smb.i2cWrite (0x24, 1, Buffer.from([0x00]));
         await delay(1000);
       }
     } catch (e) {
